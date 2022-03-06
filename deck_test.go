762a38c37b8a14a384cd82b27a7f66ff50692369
jibinraj <jibinrajck@gmail.com>
2022-03-06 11:37:02 +1100

    Added new test cases for 'Save to File'

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 
@@ -9,4 +12,22 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected length of 52 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected length of 52 but got %v", len(loadedDeck))
+	}
+
 }
